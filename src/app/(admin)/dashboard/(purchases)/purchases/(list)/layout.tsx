94fd163c85863c15a3aef314093faa4063bbe9ea
DESKTOP-DCPA18G\Dayler <daylersan@gmail.com>
2025-05-30 16:34:59 -0500

    feat: update purchase data to include subtotal, discount, and total amount

---
+++ src/app/(admin)/dashboard/(purchases)/purchases/(list)/layout.tsx
@@ -0,0 +1,23 @@
+import { PageHeader } from '@/components/app/header-section'
+import { APP_URLS } from '@/config/app-urls'
+import { Plus } from 'lucide-react'
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex flex-col gap-4 p-4 bg-white dark:bg-gray-800">
+      <PageHeader
+        title="Compras realizadas"
+        description="Aquí puedes ver todas las compras realizadas por los clientes."
+        actionButton={{
+          label: 'Añadir Producto',
+          href: APP_URLS.PRODUCTS.CREATE,
+          icon: <Plus className="h-4 w-4 mr-2" />
+        }}
+      />
+      <div className="flex flex-col gap-4 container mx-auto">{children}</div>
+      <div className="mt-4">
+        {/* Footer or additional content can go here */}
+      </div>
+    </div>
+  )
+}
