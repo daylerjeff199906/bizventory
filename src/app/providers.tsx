6632afa000e9eca0ec81d5c0421383c3505daf43
DESKTOP-DCPA18G\Dayler <daylersan@gmail.com>
2025-10-26 00:50:09 -0500

    feat: add ResourcesPage and VideoCard components with related updates

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,13 +1,16 @@
 // app/providers.tsx
 'use client'
+import { ThemeProvider } from 'next-themes'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <ToastContainer theme="dark" />
-      {children}
+      <ThemeProvider attribute="class" enableSystem={true} defaultTheme="light">
+        <ToastContainer theme="dark" position="bottom-right" />
+        {children}
+      </ThemeProvider>
     </>
   )
 }
