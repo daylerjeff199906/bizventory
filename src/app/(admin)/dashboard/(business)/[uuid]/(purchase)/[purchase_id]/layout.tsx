cb8ec90976e974d94d4277468b4b47a48e26cb12
unknown <UNAP@economicoslap05.postgradounap.edu.pe>
2025-11-04 11:59:35 -0500

    feat: agregar componente de diseño para la página de detalles de compra

---
+++ src/app/(admin)/dashboard/(business)/[uuid]/(purchase)/[purchase_id]/layout.tsx
@@ -0,0 +1,31 @@
+import { PageHeader } from '@/components/app/header-section'
+import { LayoutWrapper } from '@/components/layouts'
+import { APP_URLS } from '@/config/app-urls'
+import { Params } from '@/types'
+
+interface LayoutProps {
+  params: Params
+  children: React.ReactNode
+}
+
+export default async function Layout(props: LayoutProps) {
+  const { children } = props
+  const params = await props.params
+  const uuid = params.uuid
+
+  return (
+    <LayoutWrapper>
+      <PageHeader
+        title="Detalles de Compra"
+        description="Aquí puedes ver los detalles de la compra, incluyendo los productos adquiridos y sus cantidades."
+        backButton={{
+          href: APP_URLS.ORGANIZATION.PURCHASES.LIST(uuid?.toString() || ''),
+          hidden: false
+        }}
+      />
+      <div className="flex flex-col gap-4 container max-w-5xl mx-auto">
+        {children}
+      </div>
+    </LayoutWrapper>
+  )
+}
