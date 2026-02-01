18347de48d8dc445076d9bf92b0d64ac7a540039
DESKTOP-DCPA18G\Dayler <daylersan@gmail.com>
2026-02-01 16:59:00 -0500

    fixed logout

--- src/components/panel-admin/profile-menu.ts
+++ src/components/panel-admin/profile-menu.ts
@@ -1,30 +1,17 @@
 'use client'
 import { MenuSection } from './interfaces.profile.popover'
-// import { APP_URLS } from '@/config/app-urls'
-// import { handleLogout } from '@/utils/logout'
+import { signOut } from '@/apis/core/auth'
 
 export const MENU_PROFILE_USER: MenuSection[] = [
   {
     label: 'Opciones',
     items: [
-      // {
-      //   label: 'Panel de control',
-      //   href: APP_URLS.DASHBOARD.BASE
-      // },
-      // {
-      //   label: 'Perfil',
-      //   href: APP_URLS.DASHBOARD.PROFILE
-      // },
-      // {
-      //   label: 'Configuración',
-      //   href: APP_URLS.DASHBOARD.SETTINGS
-      // },
-      // {
-      //   label: 'Cerrar sesión',
-      //   onClick: () => {
-      //     handleLogout(APP_URLS.AUTH.LOGIN)
-      //   }
-      // }
+      {
+        label: 'Cerrar sesión',
+        onClick: async () => {
+          await signOut()
+        }
+      }
     ]
   }
 ]
