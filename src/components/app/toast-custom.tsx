f793afa3276ef8a9e8e607fd62429e5e06f4b31f
DESKTOP-DCPA18G\Dayler <daylersan@gmail.com>
2026-02-14 01:16:20 -0500

    feat: add loading pages

--- src/components/app/toast-custom.tsx
+++ src/components/app/toast-custom.tsx
@@ -1,15 +1,16 @@
 interface ToastCustomProps {
   title: string
-  message: string
+  message?: string
+  description?: string
 }
 
 export const ToastCustom = (props: ToastCustomProps) => {
-  const { title, message } = props
+  const { title, message, description } = props
 
   return (
     <main className="flex flex-col gap-1">
       <h1 className="text-sm font-bold">{title}</h1>
-      <p className="text-xs ">{message}</p>
+      {(message || description) && <p className="text-xs ">{message || description}</p>}
     </main>
   )
 }
